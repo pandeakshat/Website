3f743a069e144746f4bcbe52747c4e156ec78cf1
Akshat <akshat.pande@outlook.com>
2024-03-06 13:58:54 +0000

    Update one

---
+++ src/content/certifications/Google Data Analytics Professional Certificate.md
@@ -0,0 +1,9 @@
+---
+title: "Google Data Analytics Professional Certificate"
+publishedAt: 2023-05-24
+description: "Google Data Analytics Professional Certificate"
+slug: "Google Data Analytics Professional Certificate"
+isPublish: true
+---
+
+## Hello World
---
+++ src/data/testimonials.ts
@@ -0,0 +1,25 @@
+export type Testimonial = {
+  title: string;
+  text: string;
+};
+
+const testimonials: Testimonial[] = [
+  {
+    title: "Nikola Marciniak",
+    text: "Akshat has done an exemplary job as a Customer Service Advisor - he has continuously demonstrated a high level of customer service, punctuality, attention to detail and problem-solving skills. He has been able to successfully handle difficult encounters in a professional manner. Additionally, he has ALWAYS been willing to go the extra mile to ensure that our students are safe and are enjoying their stay with us. ",
+  },
+  {
+    title: "Nishtha Srivastava",
+    text: "Akshat is one of the most dedicated individual I've ever encountered. I have seen him go above and beyond and put in extraordinary efforts to achieve his goals. He also has excellent competitive coding skills and a passion to achieve something big. He is a great human being who always motivates and helps others whether it is in life or academics.",
+  },
+  {
+    title: "Paristisha Gupta",
+    text: "Akshat has consistently demonstrated their ability to translate data-driven findings into actionable business strategies, showcasing their practical approach to data science. Their collaborative nature and effective communication skills have made them a valuable asset to our team.",
+  },
+  {
+    title: "Garvit Sharma",
+    text: "Akshat has consistently demonstrated their ability to lead and manage data science projects effectively. They excel in translating complex data findings into actionable insights, enabling informed decision-making at every level of the organization. Their talent for guiding cross-functional teams and fostering collaboration has greatly contributed to successful project outcomes. They have a proven track record of managing resources efficiently and ensuring projects are completed on time and within budget.",
+  },
+];
+
+export default testimonials;
